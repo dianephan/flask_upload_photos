c898c806052d02448171a38f1eaaf782834b2794
dIANE phan <dianephan12@gmail.com>
2021-02-09 11:25:06 -0800

    starter code

---
+++ settings.py
@@ -0,0 +1,6 @@
+# this is the hardcoded database of people 
+KNOWN_PARTICIPANTS = {
+  'herooftime@hyrule.com': "<YOUR_PHONE_NUMBER>",
+  'zelda@hyrule.com': '+15551234567',
+  'tetra@hyrule.com': '+15557654321'
+}
