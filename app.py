c3f988783e0210839e1f5490040fd3855d564503
dIANE phan <dianephan12@gmail.com>
2021-03-11 16:04:35 -0800

    refactored app.py to work

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+# import required libraries 
+from __future__ import print_function 
+# # import Image from wand.image module 
+# from wand.image import Image # do i need this? 
+
 import os
 from dotenv import load_dotenv
 import sqlite3
@@ -8,130 +13,111 @@ from werkzeug.datastructures import FileStorage
 
 load_dotenv()
 app = Flask(__name__)
-app.secret_key = 'dsfdsd'
+app.secret_key = 'ewdsdasasdfas'
 
 app.config['UPLOAD_EXTENSIONS'] = ['jpg', 'png']
 
-def allowed_file(filename):
-   return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['UPLOAD_EXTENSIONS']
+# Create a function that converts a digital file into binary
+def convert_into_binary(file_path):
+  print("[INFO] : converting into binary data rn")
+  with open(file_path, 'rb') as file:
+    binary = file.read()
+  return binary
 
-def convertToBinaryData(filename):
-    print("[INF0] : about to convert to binary data")
-    # Convert digital data to binary format
-    with open(filename, 'rb') as file:
-        blob_data = file.read()
-    return blob_data
+# i do not need this shit 
+# def write_to_file(binary_code, file_name):
+#   # Convert binary to a proper file and store in memory
+#   print("[INFO] : writing to file rn")
+#   with open(file_name, 'wb') as file:
+#     file.write(binary_code)
+#   print(f'Created file with name: {file_name}')
 
-# dont need to do this if i dont have to??
-def writeTofile(data, filename):
-    # Convert binary data to proper format and write it on Hard Disk
-    with open(filename, 'wb') as file:
-        file.write(data)
-    print("Stored blob data into: ", filename, "\n")
+# code above copied from another article
+
+# fuck yeah this function works and writes to file 
+def writeTofile(binarydata, filename):
+  # Convert binary data to proper format and write it on Hard Disk
+  with open(filename, 'wb') as file:
+    file.write(binarydata)
+  print("[DATA] : Stored blob data into: ", filename, "\n")
 
 def readBlobData(entryID):
-    try:
-        conn = sqlite3.connect('app.db')
-        cur = conn.cursor()
-        print("Connected to SQLite")
+  try:
+    conn = sqlite3.connect('app.db')
+    cur = conn.cursor()
+    print("Connected to SQLite to readBlobData")
 
-        sql_fetch_blob_query = """SELECT * from uploads where id = ?"""
-        cur.execute(sql_fetch_blob_query, (entryID,))
-        record = cur.fetchall()
-        for row in record:
-            print("Id = ", row[0])
-            photo  = row[1]
-        print("Storing employee image and resume on disk \n")
+    sql_fetch_blob_query = """SELECT * from uploads where id = ?"""
+    cur.execute(sql_fetch_blob_query, (entryID,))
+    record = cur.fetchall()
+    for row in record:
+      print("[DATA] : row = ", row)
+      print("Id = ", row[0])
+      photo_binarycode  = row[1]
+      dud_file_name = "random file name hereee"
 
-
-        # photoPath = "E:\pynative\Python\photos\db_data\\" + name + ".jpg"
-        photoPath = "/Users/diane/Desktop/pythonfiles" + photo + ".jpg"
-        print("[INFO] : photopath = ", photoPath)
-        writeTofile(photo, photoPath)
-        cur.close()
-    except sqlite3.Error as error:
-        print("Failed to read blob data from sqlite table", error)
-    finally:
-        if conn:
-            conn.close()
-
+      # write to file???? 
+      writeTofile(photo_binarycode, dud_file_name)
+    print("[DATA] : Storing employee image and resume on disk \n")
+    cur.close()
+  except sqlite3.Error as error:
+    print("Failed to read blob data from sqlite table", error)
+  finally:
+    if conn:
+        conn.close()
 
 
+# code below is my attempt 
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    # print("[INFO] : reading blobdata for 3")
-    # readBlobData(3)
-    # TypeError: can only concatenate str (not "bytes") to str
+  # TypeError: can only concatenate str (not "bytes") to str
+  filepathname = "/Users/diane/Desktop/myvmk/Screen Shot 2021-01-11 at 10.46.01 PM.png"
+  filename_blob = convert_into_binary(filepathname)
+  print("[INFO] : the last 100 characters of blob = ", filename_blob[:100]) 
+  try:
+    conn = sqlite3.connect('app.db')
+    print("Successful connection!")
+    cur = conn.cursor()
+    insert_file = '''INSERT INTO uploads(file_names)
+      VALUES(?)'''
+    cur = conn.cursor()
+    cur.execute(insert_file, (filename_blob,))
+    conn.commit()
+    print("[INFO] : the blob for ", filepathname, " is inside uploadfolder") 
+    return render_template('success.html')
+  except Error as e:
+    print(e)
+  finally:
+    if conn:
+      conn.close()
+    else:
+      error = "Please upload a valid file."
+  return "your VMK file was supposed to be uploaded. hope it did. "
 
-    print("[INFO] : reading blobdata for 4")
-    readBlobData(4)
-    # TypeError: can only concatenate str (not "bytes") to str
-    # filepathname = "/Users/diane/Desktop/myvmk/Screen Shot 2021-01-11 at 10.46.01 PM.png"
-        
-    # filename_blob = convertToBinaryData(filepathname)
-    # # filename_blob = convertToBinaryData(f.filename)
-    # print("[INFO] : the blob = ", filename_blob) 
-    # try:
-    #     conn = sqlite3.connect('app.db')
-    #     print("Successful connection!")
-    #     cur = conn.cursor()
-    #     insert_file = '''INSERT INTO uploads(file_names)
-    #         VALUES(?)'''
-    #     cur = conn.cursor()
-    #     cur.execute(insert_file, (filename_blob,))
-    #     conn.commit()
-    #     print("[INFO] : the blob for ", filepathname, " is inside uploadfolder") 
-    #     return render_template('success.html')
-    # except Error as e:
-    #     print(e)
-    # finally:
-    #     if conn:
-    #         conn.close()
-    #     else:
-    #         error = "Please upload a valid file."
-    # return render_template('index.html', error=error)
+@app.route('/viewfiles', methods = ['GET', 'POST'])
+def see_files():
+  try:
+    conn = sqlite3.connect('app.db')
+    print("Successful connection!")
+    cur = conn.cursor()
+    # time to retrieve file 
+    user_id = 1
+    sql_retrieve_file_query = f"""SELECT * FROM uploads WHERE id = ?"""   
+    cur.execute(sql_retrieve_file_query, (user_id,))
+    record = cur.fetchone()
+    record_blob = record[1]
+    print("[INFO] : the last 100 of record_blob =  ", record_blob[:100]) 
+    
+    print("[INFO] : reading blobdata of 1 now")
+    readBlobData(1)
 
-    # use the line below for end project 
-#    return render_template('index.html')
-
-
-
-
-@app.route('/uploader', methods = ['GET', 'POST'])
-def submitted_file():
-   if request.method == 'POST':
-      f = request.files['file']
-    #   print("[INFO] : the file = ", ) 
-
-      if f and allowed_file(f.filename):  
-        # f.save(secure_filename(f.filename))       # do not do this otherwise it saves on your proj directory
-        print("[INFO] : f.filename = ", f.filename)
-        # if you pass f.filename into converBD, it doesnt work bc u need the entire path name
-
-        filename_blob = convertToBinaryData(f.filename)
-        try:
-          conn = sqlite3.connect('app.db')
-          print("Successful connection!")
-          cur = conn.cursor()
-          insert_file = '''INSERT INTO uploads(file_names)
-              VALUES(?)'''
-          cur = conn.cursor()
-          cur.execute(insert_file, (filename_blob,))
-          conn.commit()
-          print("[INFO] : the blob for ", f.filename, " is inside uploadfolder") 
-          return render_template('success.html')
-        except Error as e:
-          print(e)
-        finally:
-          if conn:
-            conn.close()
-      else:
-        error = "Please upload a valid file."
-        return render_template('index.html', error=error)
-
-# probably wont do this if i don't have to 
-@app.route('/uploads', methods=['GET', 'POST'])
-def show_files():
-   return render_template('uploads.html')
+  except Error as e:
+    print(e)
+  finally:
+    if conn:
+      conn.close()
+    else:
+      error = "how tf did u get here."
+  return "lets display your vmk pic here "
 
